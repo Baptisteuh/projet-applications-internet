b942685201c85a67c552423ae9db660fe5c8a46d
Baptisteuh <77227564+Baptisteuh@users.noreply.github.com>
2023-12-08 19:54:10 +0100

    Class to functions and delete refreshing page when changing

--- src/base/pages/amalgame.js
+++ src/base/pages/amalgame.js
@@ -4,35 +4,30 @@ import './amalgame.css'
 import { Grid, Item, Button } from '@mui/material'
 import amalgameIcon from '../../assets/images/amalgame.png';
 import fuseButtonActivate from '../../assets/images/fuseButtonActivate.png';
-import fuseButtonDeactivate from '../../assets/images/fuseButtonDeactivate.png';
 import AmalgameCase from '../components/amalgameCase/amalgameCase';
 import AmalgameFilter from '../components/amalgameFilter/amalgameFilter';
 
-class Amalgame extends React.Component {
+export default function Amalgame() {
 
-    render() {
-        return <div id="contentPage">
-            <Grid container 
-            spacing={0}
-            >
-                <Grid item xs={4}>
-                    <AmalgameCase idItem="2" />
-                    <AmalgameFilter />
-                </Grid>
-                <Grid item xs={4}>
-                    <Button text="Fabriquer"/>
-                    <div id="containerAmalgameIcon">
-                        <img src={amalgameIcon} id="amalgameIcon" />
-                        <img src={fuseButtonActivate} id="fuseButtonIcon" />
-                    </div>
-                </Grid>
-                <Grid item xs={4}>
-                    <AmalgameCase idItem="1" />
-                    <AmalgameFilter />
-                </Grid>
+    return (<div id="contentPage">
+        <Grid container 
+        spacing={0}
+        >
+            <Grid item xs={4}>
+                <AmalgameCase idItem="2" />
+                <AmalgameFilter />
             </Grid>
-        </div>
-    }
+            <Grid item xs={4}>
+                <Button text="Fabriquer"/>
+                <div id="containerAmalgameIcon">
+                    <img src={amalgameIcon} id="amalgameIcon" />
+                    <img src={fuseButtonActivate} id="fuseButtonIcon" />
+                </div>
+            </Grid>
+            <Grid item xs={4}>
+                <AmalgameCase idItem="1" />
+                <AmalgameFilter />
+            </Grid>
+        </Grid>
+    </div>)
 }
-
-export default Amalgame
