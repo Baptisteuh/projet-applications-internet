46226a9bbe8ee2218989946efa07bf646665a71d
IcePingus <pingus32@hotmail.fr>
2023-12-09 01:48:13 +0100

    Add images

--- src/base/components/amalgameCase/amalgameCase.js
+++ src/base/components/amalgameCase/amalgameCase.js
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import background from "../../../assets/images/fuseItemBackground.png"
-import testSword1 from "../../../assets/images/items/testSword1.png"
-import testSword2 from "../../../assets/images/items/testSword2.png"
+import testSword1 from "../../../assets/images/items/boulder-breaker-weapon-zelda-totk-wiki-guide.png"
+import testSword2 from "../../../assets/images/items/bomb_flower_materials_zelda_tears_of_the_kingdom_wiki_guide_200px.png"
 import './amalgameCase.css'
 
 class AmalgameCase extends React.Component {
   render() {
     return  <div class="image-container" id="container">
     <img src={background} />
-    {this.props.idItem == "2" ? <img class="itemCase" src={testSword2} />:<div></div>}
-    {this.props.idItem == "1" ? <img class="itemCase" src={testSword1} />:<div></div>}
+    {this.props.idItem == "2" ? <img class="itemCase" src={testSword1} />:<div></div>}
+    {this.props.idItem == "1" ? <img class="itemCase" src={testSword2} />:<div></div>}
             </div>
   }
 }
